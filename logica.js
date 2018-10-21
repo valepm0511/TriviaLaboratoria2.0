1bd45026ad3cb84aa330ca9d41b23a1bd9966ba7
valeria <valeria.penaloza5@gmail.com>
2018-10-21 01:18:57 -0300

    corrigiendo trivia

--- logica.js
+++ logica.js
@@ -1,62 +1,68 @@
 window.logica = {};
 
 window.logica.yes = () => {
-    let contentName = document.getElementById('contentName');
-    contentName.innerHTML = `
-    <div class="block">
-        <p>Ingresa tu nombre:</p>
-        <input type="text" class="form-control" id="name">
-        <button type="button" class="btn btn-warning mt-2" name="enviar" value="enviar" onclick="window.logica.nameUser()">Enviar</button>
-    </div>
-    `;
-    window.logica.nameUser();
+    document.getElementById('contentQuestion').classList.remove('displayNone');
+    document.getElementById('contentQuestion').classList.add('displayBlock');
+    window.logica.question();
 }
 
-window.logica.nameUser = () => {
+window.logica.question = () => {
     let questionName = document.getElementById('name').value;
-    if (questionName.length === 0) {
+    document.getElementById('contentName').classList.remove('displayNone');
+    document.getElementById('contentName').classList.add('displayBlock');
+    document.getElementById('contentQuestion').classList.add('displayNone');
+    document.getElementById('contentQuestion').classList.remove('displayBlock');
+    if (questionName.length == 0) {
         alert("Porfavor ingrese un nombre");
     } else {
-        let contentName = document.getElementById('contentName');
-        contentName.innerHTML = `
-        <p class = "text-center">${questionName} empecemos a jugar</p>
-        <div class="form-check">
-            <form name="formQuestion ">
-                <div id="question1 ">
-                <p>¿Que lenguaje de programacion enseñan en Laboratoria?</p>
-                    <input class="form-check-input" type="radio" value="css" name="firstQuestion">
-                    <label class="form-check-label">CSS</label> </br>
-                    <input class="form-check-input" type="radio" value="javascript" name="firstQuestion">
-                    <label class="form-check-label"> Javascript</label> </br>
-                    <input class="form-check-input" type="radio" value="php" name="firstQuestion">
-                    <label class="form-check-label">PHP</label></br>
-                    <input class="form-check-input" type="radio" value="c++" name="firstQuestion">
-                    <label class="form-check-label">C++</label></br>
-                    <input class="form-check-input" type="radio" value="java" name="firstQuestion">
-                    <label class="form-check-label">Java</label></br>
-                </div>
-                <div id="question2 ">
-                <p>¿Cuantos meses dura el bootcamp de Laboratoria?</p>
-                    <input class="form-check-input" type="radio" value="2" name="secondQuestion">
-                    <label class="form-check-label">2</label></br>
-                    <input class="form-check-input" type="radio" value="4" name="secondQuestion">
-                    <label class="form-check-label">4</label></br>
-                    <input class="form-check-input" type="radio" value="6" name="secondQuestion">
-                    <label class="form-check-label">6</label></br>
-                    <input class="form-check-input" type="radio" value="8" name="secondQuestion">
-                    <label class="form-check-label">8</label></br>
-                    <input class="form-check-input" type="radio" value="10" name="secondQuestion">
-                    <label class="form-check-label">10</label></br>
-                </div>
-            </form>
-        </div> `;
+        let goPlay = document.getElementById('goPlay');
+        goPlay.innerHTML = `
+        Bien ${questionName}, entonces empecemos!
+        `;
+        document.getElementById('welcome').classList.remove('displayBlock');
+        document.getElementById('welcome').classList.add('displayNone');
+        document.getElementById('contentName').classList.add('displayNone');
+        document.getElementById('contentName').classList.remove('displayBlock');
+        document.getElementById('contentFormQuestion').classList.remove('displayNone');
+        document.getElementById('contentFormQuestion').classList.add('displayBlock');
+    }
+}
+
+window.logica.answers = () => {
+    document.getElementById('contentFormQuestion').classList.remove('displayNone');
+    document.getElementById('contentFormQuestion').classList.add('displayBlock');
+    let answer1 = document.formQuestion.firstQuestion.value;
+    let answer2 = document.formQuestion.secondQuestion.value;
+    console.log(answer2);
+    console.log(answer1);
+    let score1 = 0;
+    let score2 = 0;
+    document.getElementById('contentAnswer').classList.remove('displayNone');
+    document.getElementById('contentAnswer').classList.add('displayBlock');
+    document.getElementById('contentAnswer');
+    // let answerRight = document.getElementById('answerRight');
+    // let answerWrong = document.getElementById('answerWrong');
+
+    let scoreTotal = score1 + score2;
+    if (answer1 == "javascript") {
+        contentAnswer.innerHTML += `<p>Correcto, enseña JavaScript </p>`;
+        score1 += 100;
+    } else {
+        contentAnswer.innerHTML += `<p>Lastima te equivocaste, enseña JavaScript </p>`;
+    }
+
+    if (answer2 == "6") {
+        contentAnswer.innerHTML += `<p>Correcto, son 6 meses </p>`;
+        score2 += 100;
+    } else {
+        contentAnswer.innerHTML += `<p>Lastima te equivocaste, son 6 meses </p>`;
     }
 
 }
 
 window.logica.no = () => {
-    let contentName = document.getElementById('contentName');
-    contentName.innerHTML = `
+    let contentQuestion = document.getElementById('contentQuestion');
+    contentQuestion.innerHTML = `
     <p class="text-center display-4 text-success">:'(</p>
     <div class="d-flex justify-content-center">
         <button type="button" class="btn btn-warning mt-2" name="volver" value="volver" onclick="location.reload()">Volver</button>
